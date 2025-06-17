8d79d16bd9194d75bac92335daffaba9fb1bf743
Martin <you@example.com>
2025-06-17 16:50:01 +0200

    chagned comment

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import { initGrids } from "./state"
 initGrids()
 setPattern(1)
 runGameLoop()
-drawGrid() //called in runGameLoop as well, but needs to be called once to draw the grid
+drawGrid() //called in runGameLoop as well, but needs to be called once to draw the grid.
