5e3cd25369672efa686d5b7fa5faf709804ee345
sampleCoin <martin.hustoles@gmail.com>
2025-06-28 18:16:53 +0200

    updated tailwind config, minor css update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  corePlugins: {
+    container: false,
+    float: false,
+  },
   theme: {
     extend: {
       screens: {
