540420b5ed217e767f5d641c30ea3f61308dd03f
sampleCoin <martin.hustoles@gmail.com>
2025-06-29 22:27:28 +0200

    added https cert

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, '/etc/letsencrypt/live/martinhustoles.com/privkey.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, '/etc/letsencrypt/live/martinhustoles.com/fullchain.pem'))
+        },
         allowedHosts: [
-        'martinhustoles.com', // Your production domain
-        'localhost',          // Local development
-        '0.0.0.0'             // Docker internal
+            'martinhustoles.com', // Your production domain
+            'localhost',          // Local development
+            '0.0.0.0'             // Docker internal
         ],
         host: '0.0.0.0',        // Required for Docker
         strictPort: true
